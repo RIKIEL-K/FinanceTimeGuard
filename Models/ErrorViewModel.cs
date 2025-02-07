db678369380d0960382f117f6deee7eebea5d34b
RIKIEL-K <rikkouambeng05@gmail.com>
2025-02-07 10:27:51 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace prjFinanceTime.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
