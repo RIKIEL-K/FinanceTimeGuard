37e7c85afcf86e08edffa2aeea3478bce6a13f02
RIKIEL-K <rikkouambeng05@gmail.com>
2025-02-07 11:30:14 -0500

    Add of the User and Model Entity

--- Models/Image.cs
+++ Models/Image.cs
@@ -1,6 +1,23 @@
-﻿namespace prjFinanceTime.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace prjFinanceTime.Models
 {
     public class Image
     {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Path { get; set; }
+
+        [NotMapped] //pas stocker dans la DB
+        [Display(Name  = "Image")]
+        //represente le fichier stocké sur la DBA
+        public IFormFile file { get; set; }
+        public virtual User user { get; set; }
+
+        public int FruitId { get; set; }
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,33 @@
-﻿namespace prjFinanceTime.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace prjFinanceTime.Models
 {
     public class User
     {
+        public int UserId {  get; set; }
+
+        [StringLength(50)]
+        [Required]
+        [Display(Name = "Name")]
+        public string UserName { get; set; }
+
+        [StringLength(50)]
+        [Required]
+        [Display(Name = "email")]
+        [EmailAddress]
+        public string UserEmail { get; set; }
+
+        [StringLength(50)]
+        [Required]
+        [Display(Name = "password")]
+        [PasswordPropertyText]
+        public string Password { get; set; }
+
+        [StringLength(50 , MinimumLength = 5)]
+        [Required]
+        [Display(Name = "createdAt")]
+        public DateTime createdAt  { get; set; }
+        public virtual Image Image { get; set; }
     }
 }
